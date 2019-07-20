ecf5aedaeb79d88283b485a457dfdae1248b7e6d
Alex Brioux <abrioux25@gmail.com>
2019-07-20 00:54:50 -0700

    Top bar of pokedex

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -2,18 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { PokemonSpotlight } from '../PokemonSpotlight'
+import { Pokedex } from '../Pokedex'
 
 const AppContainer = styled.div`
   text-align: center;
+  padding: 50px;
 `
 
 export const Root = () => {
-  let pokemon = []
-  for (let i = 1; i <= 151; ++i) {
-    pokemon.push(<PokemonSpotlight id={i} />)
-  }
+  // let pokemon = []
+  // for (let i = 1; i <= 151; ++i) {
+  //   pokemon.push(<PokemonSpotlight id={i} />)
+  // }
 
-  return <AppContainer>{pokemon}</AppContainer>
+  // return <AppContainer>{pokemon}</AppContainer>
+  return (
+    <AppContainer>
+      <Pokedex />
+    </AppContainer>
+  )
 }
 
 Root.displayName = 'Root'
